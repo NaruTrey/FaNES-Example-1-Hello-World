34f50b45f5c059744688a929bd18138a71b3c68d
Andrey K <NaruTrey@users.noreply.github.com>
2015-12-16 12:21:23 +0400

    Set the background color

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <FaNES.h>
 
 void main(void) {
+    setPaletteColor(PALETTE_BACKGROUND_1 + 0, 0x0F);
     setPaletteColor(PALETTE_BACKGROUND_1 + 3, 0x30);
     printNametableString(NAMETABLE_A_POSITION(2, 2), "Hello World!");
     onGraphics();
