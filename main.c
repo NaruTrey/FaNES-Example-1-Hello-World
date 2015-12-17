4ffb20d8acb66d61c2da1ddc5b255e5555b8bea1
NaruTrey <me@ntrey.org>
2015-12-17 17:19:29 +0400

    Remove Cyrillic

--- main.c
+++ main.c
@@ -1,13 +1,8 @@
 #include <FaNES.h>
 
-/*!!!
- * Обязательно пересохраните файл в кодировке KOI8R, иначе вместо кириллицы будут кракозябры
- */
 void main(void) {
     setPaletteColor(PALETTE_BACKGROUND_1 + 3, 0x30);
     printNametableString(NAMETABLE_A_POSITION(2, 2), "Hello World!");
-    printNametableString(NAMETABLE_A_POSITION(2, 4), "Привет мир! Этот текст");
-    printNametableString(NAMETABLE_A_POSITION(2, 6), "должен быть в кодировке KOI8R.");
     onGraphics();
     while(TRUE) {
         waitFrame();
